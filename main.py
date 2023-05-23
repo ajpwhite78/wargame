9fe0f174801247badb89619b8467b51258c8d073
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-23 12:26:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,32 @@ import base64
 import os
 from apps import manual_analysis, simulation_analysis
 
+def set_page_title(title):
+    st.sidebar.markdown(unsafe_allow_html=True, body=f"""
+        <iframe height=0 srcdoc="<script>
+            const title = window.parent.document.querySelector('title') \
+                
+            const oldObserver = window.parent.titleObserver
+            if (oldObserver) {{
+                oldObserver.disconnect()
+            }} \
+
+            const newObserver = new MutationObserver(function(mutations) {{
+                const target = mutations[0].target
+                if (target.text !== '{title}') {{
+                    target.text = '{title}'
+                }}
+            }}) \
+
+            newObserver.observe(title, {{ childList: true }})
+            window.parent.titleObserver = newObserver \
+
+            title.text = '{title}'
+        </script>" />
+    """)
+
+
+set_page_title("My new title")
 
 st.set_page_config(page_title="Paydar", page_icon="images/Favicon.png", layout="wide")
 
