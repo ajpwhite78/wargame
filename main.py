e6de192928ea35a9a6a78f69149e19932a364f69
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-24 15:09:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -698,7 +698,7 @@ media_query = '''
         p {
             font-size: 2.4em;
             margin-top: 1.25em;
-            border-width: 0.8em;
+            border: 0.5em;
         }
     }
     </style>
