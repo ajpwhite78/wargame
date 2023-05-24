6d99fe99fd0c20d8e9421016a72ed6e9d2b01661
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-24 15:40:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -698,8 +698,10 @@ media_query = '''
     <style>
     @media (max-width: 600px) {
         .introduction-text {
+        p {
             font-size: 2.4em;
             border: 0.5em solid #25476A !important;
+        }
         }
     }
     </style>
