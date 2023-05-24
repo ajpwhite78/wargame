b02b2362df3cc7f7652d51695efa0ba6444f848f
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-24 13:46:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -447,7 +447,7 @@ header3 = """
 
         .welcome-container {{
             width: 50%;  /* Set width to 100% for full width on smaller screens */
-            font-size: 3em;
+            font-size: 2.4em;
             justify-content: center;  /* Center align items on smaller screens */
             margin-bottom: 0.5em;  /* Adjust margin for smaller screens */
         }}
