500a1857a0ce0a67139c4863adf35c0520e9c317
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-24 18:48:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -728,7 +728,7 @@ subtext1A = '<p style="margin-bottom: 0em;"><span style="font-family:sans-serif;
 st.markdown(subtext1A, unsafe_allow_html=True)
 st.markdown(line_media_query + line, unsafe_allow_html=True)
 st.markdown(line_media_query2 + line2, unsafe_allow_html=True)
-instructions_text = '<p style="margin-top: 0em; margin-bottom: 0em; text-align: justify;"><span style="font-family:sans-serif; color:#25476A; font-size: 1em;">Use the dropdown menus to select the sector, company name and reporting period of the company you want to analyze. Click "Run" once you have made your selections or click "Cancel" to reset.</span></p>'
+instructions_text = '<p style="margin-top: -1em; margin-bottom: 1em; text-align: justify;"><span style="font-family:sans-serif; color:#25476A; font-size: 1em;">Use the dropdown menus to select the sector, company name and reporting period of the company you want to analyze. Click "Run" once you have made your selections or click "Cancel" to reset.</span></p>'
 st.markdown(instructions_text, unsafe_allow_html=True)
 col1, col2, col3, col4, col5, col6 = st.columns([1, 2, 1, 1, 0.5, 0.5])
 with col1:
