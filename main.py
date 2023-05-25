57776f9ba325afbdc7bf6858b4af12f934a316a0
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-25 12:36:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -671,7 +671,7 @@ input[type=number] {
   color: #25476A;
   background-color: rgba(3, 169, 244, 0.2);
   border: 0.1875em solid #25476A;
-  font-size: 1.125em;
+  font-size: 1em;
   font-weight: bold;
 }
 
