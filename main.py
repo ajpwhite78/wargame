db046a2b166cfbf531594132dcf2be761f364f51
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-25 17:41:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -555,7 +555,7 @@ st.markdown(spinner_css, unsafe_allow_html=True)
 
 image_center = """
 <style>
-    .image-container {
+    .image-container {{
         display: inline-block;
         width: 30%;
         text-align: center;
@@ -564,7 +564,7 @@ image_center = """
         left: 50%;
         transform: translate(-50%, -50%);
         z-index: 9999;
-    }
+    }}
 </style>
 <div class="image-container">
     <img src="data:image/png;base64,{}" class="img-fluid" alt="logo" width="30%">
