751f5ac9ba4b30fb3fde25e25b619b93109bb98a
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-26 09:40:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -483,8 +483,8 @@ header3 = """
         .button {{
             border-width: 0.1875em;  /* Convert 3px to em units */
             border-radius: 0.1875em;  /* Convert 3px to em units */
-            font-size: 1em;  /* Convert 16px to em units */
-            width: 5em;
+            font-size: 1.4em;  /* Convert 16px to em units */
+            width: 8em;
             height: 2em;
         }}
     }}
