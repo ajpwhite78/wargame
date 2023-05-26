24f2aab35268fce43eab61ea64e9a428dc7a1b09
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-26 12:21:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -386,7 +386,7 @@ header3 = """
             display: inline-block;
             vertical-align: middle
         }}
-        .button1-div {{
+        .button-div {{
             display: flex;
             justify-content: flex-start;
             align-items: center;
@@ -409,7 +409,7 @@ header3 = """
             width: 5em;
             height: 2em;
         }}
-        .button1:hover {{
+        .button:hover {{
             background-color: #b8d9e8;
             border-color: #25476A;
             color: #25476A;
@@ -477,7 +477,7 @@ header3 = """
             justify-content: center;  /* Center align items on smaller screens */
             margin-bottom: 0.5em;  /* Adjust margin for smaller screens */
         }}
-        .button1-div {{
+        .button-div {{
             width: 50%;  /* Set width to 100% for full width on smaller screens */
             justify-content: center;  /* Center align items on smaller screens */
             gap: 2em;  /* Convert 20px to em units */
@@ -501,10 +501,10 @@ header3 = """
         <div class="clear"></div>
         <div class="welcome-container">
             <div class="welcome-text">Welcome John</div>
-            <div class="button1-div">
-                <button class="button1">Help</button>
-                <!--<button class="button1">Contact</button>-->
-                <button class="button1">Logout</button>
+            <div class="button-div">
+                <button class="button">Help</button>
+                <!--<button class="button">Contact</button>-->
+                <button class="button">Logout</button>
             </div>
         </div>
         <div class="clear"></div>
