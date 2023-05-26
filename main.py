2b6afff0b096d62c81121b3256ff6f41771dc717
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-26 10:14:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -800,7 +800,7 @@ with col5:
                 border-width: 0.1875em;  /* Convert 3px to em units */
                 border-radius: 0.1875em;  /* Convert 3px to em units */
                 font-size: 8em;  /* Convert 16px to em units */
-                width: 20em;
+                width: 10em;
                 height: 2em;
                 }
             }
