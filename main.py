65be96a34db8337d5abefa7a48c116178b5eeb66
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-27 10:07:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -46,88 +46,6 @@ def add_bg_from_local(image_file):
 add_bg_from_local("images/background.jpg")
 
 
-spinner_css_update = """
-<style>
-    #spinner-container span {
-        position: absolute;
-        left: 0;
-        width: 32px;
-        height: 6px;
-        background: #2c4766;
-        border-radius: 8px;
-        transform-origin: 128px
-        transform: scale(2.2) rotate(calc(var(--i) * (360deg/ 50)));
-        animation: animateBlink 3s linear infinite;
-        animation-delay: calc(var(--i) * (3s / 50));
-        z-index: 9999;
-    }
-    @keyframes animateBlink {
-        0% {
-            background: #00eeff;
-        }
-        25% {
-            background: #2c4766;
-        }
-    }
-
-</style>
-<div id="spinner-container">
-    <span style="--i:0;"></span>
-    <span style="--i:1;"></span>
-    <span style="--i:2;"></span>
-    <span style="--i:3;"></span>
-    <span style="--i:4;"></span>
-    <span style="--i:5;"></span>
-    <span style="--i:6;"></span>
-    <span style="--i:7;"></span>
-    <span style="--i:8;"></span>
-    <span style="--i:9;"></span>
-    <span style="--i:10;"></span>
-    <span style="--i:11;"></span>
-    <span style="--i:12;"></span>
-    <span style="--i:13;"></span>
-    <span style="--i:14;"></span>
-    <span style="--i:15;"></span>
-    <span style="--i:16;"></span>
-    <span style="--i:17;"></span>
-    <span style="--i:18;"></span>
-    <span style="--i:19;"></span>
-    <span style="--i:20;"></span>
-    <span style="--i:21;"></span>
-    <span style="--i:22;"></span>
-    <span style="--i:23;"></span>
-    <span style="--i:24;"></span>
-    <span style="--i:25;"></span>
-    <span style="--i:26;"></span>
-    <span style="--i:27;"></span>
-    <span style="--i:28;"></span>
-    <span style="--i:29;"></span>
-    <span style="--i:30;"></span>
-    <span style="--i:31;"></span>
-    <span style="--i:32;"></span>
-    <span style="--i:33;"></span>
-    <span style="--i:34;"></span>
-    <span style="--i:35;"></span>
-    <span style="--i:36;"></span>
-    <span style="--i:37;"></span>
-    <span style="--i:38;"></span>
-    <span style="--i:39;"></span>
-    <span style="--i:40;"></span>
-    <span style="--i:41;"></span>
-    <span style="--i:42;"></span>
-    <span style="--i:43;"></span>
-    <span style="--i:44;"></span>
-    <span style="--i:45;"></span>
-    <span style="--i:46;"></span>
-    <span style="--i:47;"></span>
-    <span style="--i:48;"></span>
-    <span style="--i:49;"></span>
-</div>
-"""
-st.markdown(spinner_css_update, unsafe_allow_html=True)
-
-
-
 def img_to_bytes(img_path):
     img_bytes = pathlib.Path(img_path).read_bytes()
     encoded = base64.b64encode(img_bytes).decode()
@@ -609,6 +527,90 @@ with open(image_file_path, "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read()).decode()
 
 st.markdown(header3.format(encoded_string, img_to_bytes("images/Paydar-logo-white-transparent.png")), unsafe_allow_html=True)
+
+
+spinner_css_update = """
+<style>
+    #spinner-container span {
+        position: absolute;
+        left: 0;
+        width: 32px;
+        height: 6px;
+        background: #2c4766;
+        border-radius: 8px;
+        transform-origin: 128px
+        transform: scale(2.2) rotate(calc(var(--i) * (360deg/ 50)));
+        animation: animateBlink 3s linear infinite;
+        animation-delay: calc(var(--i) * (3s / 50));
+        z-index: 9999;
+    }
+    @keyframes animateBlink {
+        0% {
+            background: #00eeff;
+        }
+        25% {
+            background: #2c4766;
+        }
+    }
+
+</style>
+<div id="spinner-container">
+    <span style="--i:0;"></span>
+    <span style="--i:1;"></span>
+    <span style="--i:2;"></span>
+    <span style="--i:3;"></span>
+    <span style="--i:4;"></span>
+    <span style="--i:5;"></span>
+    <span style="--i:6;"></span>
+    <span style="--i:7;"></span>
+    <span style="--i:8;"></span>
+    <span style="--i:9;"></span>
+    <span style="--i:10;"></span>
+    <span style="--i:11;"></span>
+    <span style="--i:12;"></span>
+    <span style="--i:13;"></span>
+    <span style="--i:14;"></span>
+    <span style="--i:15;"></span>
+    <span style="--i:16;"></span>
+    <span style="--i:17;"></span>
+    <span style="--i:18;"></span>
+    <span style="--i:19;"></span>
+    <span style="--i:20;"></span>
+    <span style="--i:21;"></span>
+    <span style="--i:22;"></span>
+    <span style="--i:23;"></span>
+    <span style="--i:24;"></span>
+    <span style="--i:25;"></span>
+    <span style="--i:26;"></span>
+    <span style="--i:27;"></span>
+    <span style="--i:28;"></span>
+    <span style="--i:29;"></span>
+    <span style="--i:30;"></span>
+    <span style="--i:31;"></span>
+    <span style="--i:32;"></span>
+    <span style="--i:33;"></span>
+    <span style="--i:34;"></span>
+    <span style="--i:35;"></span>
+    <span style="--i:36;"></span>
+    <span style="--i:37;"></span>
+    <span style="--i:38;"></span>
+    <span style="--i:39;"></span>
+    <span style="--i:40;"></span>
+    <span style="--i:41;"></span>
+    <span style="--i:42;"></span>
+    <span style="--i:43;"></span>
+    <span style="--i:44;"></span>
+    <span style="--i:45;"></span>
+    <span style="--i:46;"></span>
+    <span style="--i:47;"></span>
+    <span style="--i:48;"></span>
+    <span style="--i:49;"></span>
+</div>
+"""
+st.markdown(spinner_css_update, unsafe_allow_html=True)
+
+
+
 
 
 spinner_css = """
