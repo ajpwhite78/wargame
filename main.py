5f93fa96c4201b51b46bce63797ad76c8ea11af7
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-27 16:45:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -564,9 +564,9 @@ spinner_css3 = """
             display: inline-block;
             width: 20vmin;
             height: 20vmin;
-            border: 6px solid #6f72de;
-            border-left-color: rgba(0, 0, 0, 0);
-            border-radius: 50%;
+            background: #00eeff;
+            border-radius: 8px;
+            transform-origin: 16px 50%;
             animation: spin 1s ease-in-out infinite;
         }
     }
