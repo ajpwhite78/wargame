6b872175cc55e217f6d0faa3a176b6ae2f2736df
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-27 18:54:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -538,7 +538,6 @@ spinner_css5 = """
         position: fixed;
         top: 50%;
         left: 50%;
-        transform: translate(-50%, -50%);
         width: 100%;
         height: 100%;
         z-index: 9999;
