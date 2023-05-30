15856121bbeee287678ff94f2ab18f601266457f
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-30 12:36:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -538,7 +538,7 @@ marker_spinner_css = """
         position: fixed;
         top: 0%;
         left: 0%;
-        transform: translate(54.1%, 0%);
+        transform: translate(54.15%, 0%);
         width: 100%;
         height: 100%;
         z-index: 9999;
