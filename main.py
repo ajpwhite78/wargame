c6a09d557f022277120c614f32d4efc9eab33b2e
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-31 09:23:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1839,7 +1839,7 @@ button_css2 = """
         border-radius: 0.1875em;  /* Convert 3px to em units */
         font-size: 1em;  /* Convert 16px to em units */
         width: 5em;
-        height: 2em;
+        height: 2.2em;
     }
     } 
 </style>
