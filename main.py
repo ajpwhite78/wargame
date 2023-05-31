d2302c1ea3edd5f01ec4dd9ed2a10adca12a5fa8
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-31 08:54:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1154,7 +1154,7 @@ marker_spinner_css = """
 }
 @media (max-width: 600px) {
     #spinner-container-marker {
-        transform: translate(58%, 0%);
+        transform: translate(57.5%, 0%);
     }
     .marker0 {
         width: 5em;
