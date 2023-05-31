7d5dd71eba9b26e805399ac3a3a724e4d9d1f642
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-31 09:21:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1831,7 +1831,7 @@ button_css2 = """
     .button-div2 {
         width: 100%;  /* Set width to 100% for full width on smaller screens */
         justify-content: center;  /* Center align items on smaller screens */
-        gap: 18em;
+        gap: 12em;
         margin-top: 0em;  /* Add top margin */
     }
     .button2 {
