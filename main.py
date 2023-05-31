822c3dd75e000bc33af25c8ec611410509bfcf70
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-05-31 10:05:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1835,10 +1835,7 @@ button_css2 = """
         justify-content: center;  /* Center align items on smaller screens */
         margin-top: 0em;  /* Add top margin */
     }
-    .button-container2 {
-        flex-direction: column;
-        gap: 5em;  /* Adjust the gap value as per your preference */
-    }
+
     .button2 {
         border-width: 0.1875em;  /* Convert 3px to em units */
         border-radius: 0.1875em;  /* Convert 3px to em units */
