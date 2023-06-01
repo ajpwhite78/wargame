c2a37a7cb30277544c3cb72586d6e3a702f3bb95
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-01 13:12:32 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -1170,7 +1170,7 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
         st.text("")
         col1, col2 = st.columns([5.8, 0.2])
         with col1:
-            subtext1 = '<p class="subtext" style="margin-bottom: 0em;"><span style="font-family:sans-serif; color:#25476A; font-size: 2em;">Income Statement Manual Input Fields</span></p>'
+            subtext1 = '<p class="subtext" style="margin-top: -2em; margin-bottom: 0em;"><span style="font-family:sans-serif; color:#25476A; font-size: 2em;">Income Statement Manual Input Fields</span></p>'
             text_media_query_manual2 = '''
                 <style>
                 @media (max-width: 600px) {
