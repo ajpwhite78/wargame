4d556c2de0193b8dcdef5eacfe74d06972da1eee
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-01 09:47:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ st.markdown(
         border-width: 3px;
         width: 4em;
         height: 1.8em;
-        margin-top: 20em;
+        margin-top: 4em;
     }
     div.stButton > button:hover {
         background-color: rgba(111, 114, 222, 0.6);
@@ -68,6 +68,7 @@ st.markdown(
     div.stButton > button {
         width: 90em !important;
         height: 10em !important;
+        margin-top: 0em;
         }
     }
     </style>
