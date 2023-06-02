86837181361f6dc35ea459d07be068008dd5714a
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-02 13:47:57 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -1310,11 +1310,6 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
         '''
         st.markdown(text_media_query_manual1 + text1, unsafe_allow_html=True)
 
-        
-
-
-
-
         html2 = """
         <div class="col2">
             <div class="left2">
@@ -2009,18 +2004,17 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
         left_text = "<span style='font-family: sans-serif; color: #FAFAFA; font-size: 2.25em;'>{}&nbsp;{}<span class='break-line'>&nbsp;{}</span>".format(str(st.session_state.user_reporting_period), "Rating:", "XXX")
         right_text = "<span style='font-family: sans-serif; color: #FAFAFA; font-size: 2.25em;'>{}<span class='break-line'>&nbsp;{}</span>".format("Scenario Rating:", "XXX")
         html4 = f"<div class='col'><div class='left'>{left_text}</div><div class='right'>{right_text}</div></div>"
-        st.markdown(html4, unsafe_allow_html=True)
+        st.markdown(html4, unsafe_allow_html=True)      
         
-        
-        left_text = "<span style='font-family: sans-serif; color: #FAFAFA; font-size: 2.25em;'>Manual Analysis</span>"
-        right_text = "<span style='font-family: sans-serif; color: #FAFAFA; font-size: 2.25em;'>{}<span class='break-line'>&nbsp;&nbsp;&nbsp;{}</span></span>".format(st.session_state.user_entity_name, st.session_state.user_reporting_period)
+        text1 = '''
+    <p class="text1" style="margin-top: 1em; margin-bottom: 1em; text-align: justify;"><span style="color: #25476A; background-color: rgba(3, 169, 244, 0.2); border-radius: 0.375em; padding-left: 0.75em; padding-right: 0.75em; padding-top: 0.5em; padding-bottom: 0.5em; font-family: sans-serif; font-size: 1em; font-weight: bold; display: block; width: 100%; border: 0.1875em solid #25476A;">Paydar calculates a {} rating for {} at {} and predicts a {} rating based on the scenario provided.</span></p>'.format("XXX", st.session_state.user_entity_name, st.session_state.user_reporting_period, "XXX")
+'''
+        st.markdown(text_media_query_manual1 + text1, unsafe_allow_html=True)
 
-        html = f"<div class='col'><div class='left'>{left_text}</div><div class='right'>{right_text}</div></div>"
-        st.markdown(html, unsafe_allow_html=True)
         
         
-        text = '<p style="margin-top: 20px; margin-bottom: 10px; text-align: justify;"><span style="color: #25476A; background-color: rgba(3, 169, 244, 0.2); border-radius:6px; padding-left:12px; padding-right: 12px; padding-top:12px; padding-bottom:12px; font-family:sans-serif; font-size: 24px; display: block; width: 100%; border: 3px solid #25476A; font-weight: bold;">Comrate&apos;s proprietary credit ratings model calculates a {} rating for {} at {} and predicts a {} rating based on the scenario provided.</span></p>'.format("XXX", st.session_state.user_entity_name, st.session_state.user_reporting_period, "XXX")
-        st.markdown(text, unsafe_allow_html=True)
+        
+        
         col1, col2, col3, col4, col5 = st.columns([2, 1, 1, 1, 1])
         with col1:
             st.text("")
