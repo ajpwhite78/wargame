9c8137d54d93b50694a4168b2eca48fc7034d61e
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-02 15:35:48 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2132,8 +2132,12 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
 
         col1, col2, col3, col4, col5, col6 = st.columns(6)
         with col1:
-            subtext3 = '<p style="margin-bottom: 2px;"><span style="font-family:sans-serif; color:#25476A; font-size: 24px;">Income Statement</span></p>'
-            st.markdown(subtext3, unsafe_allow_html=True)
+            left_text = "<span style='font-family: sans-serif; color: #FAFAFA; font-size: 2em;'>Income Statement</span>"
+            right_text = "<span style='font-family: sans-serif; color: #FAFAFA; font-size: 1em;'>(blue fields indicate change)</span></span>"
+
+            html5 = f"<div class='col3'><div class='left3'>{left_text}</div><div class='right3'>{right_text}</div></div>"
+            st.markdown(html5, unsafe_allow_html=True)
+            
         with col2:
             subtext3A = '<p style="margin-bottom: 2px; margin-top: 7px; text-align: right"><span style="font-family:sans-serif; color:#25476A; font-size: 16px;">(blue fields indicate change)&nbsp;&nbsp;&nbsp;&nbsp;</span></p>'
             st.markdown(subtext3A, unsafe_allow_html=True)
