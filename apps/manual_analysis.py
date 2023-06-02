e44baab46f3c759e5206544377764935aeb48c96
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-02 13:51:04 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2011,7 +2011,11 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
     '''
         st.markdown(text_media_query_manual1 + text1, unsafe_allow_html=True)
 
-        
+        text1 = '''
+        <p class="text1" style="margin-top: 1em; margin-bottom: 1em; text-align: justify;"><span style="color: #25476A; background-color: rgba(3, 169, 244, 0.2); border-radius: 0.375em; padding-left: 0.75em; padding-right: 0.75em; padding-top: 0.5em; padding-bottom: 0.5em; font-family: sans-serif; font-size: 1em; font-weight: bold; display: block; width: 100%; border: 0.1875em solid #25476A;">Paydar calculates a {} rating for {} at {} and predicts a {} rating based on the scenario provided.</span></p>'.format("XXX", st.session_state.user_entity_name, st.session_state.user_reporting_period, "XXX")
+'''
+        st.markdown(text_media_query_manual1 + text1, unsafe_allow_html=True)
+
         
         
         
