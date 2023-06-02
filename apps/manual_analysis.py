cbf811909ce5e799f924bdccfc8ab8691f221db9
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-02 13:39:29 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -1120,14 +1120,14 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
             .left {
                 text-align: left;
                 float: left;
-                width: 40%;
+                width: 50%;
                 padding-top: 0em;
                 padding-bottom: 0em;
             }
             .right {
                 text-align: right;
                 float: right;
-                width: 60%;
+                width: 50%;
                 padding-top: 0em;
                 padding-bottom: 0em;
             }
@@ -2008,16 +2008,8 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
         
         left_text = "<span style='font-family: sans-serif; color: #FAFAFA; font-size: 2.25em;'>{}&nbsp;{}&nbsp;{}</span>".format(str(st.session_state.user_reporting_period), "Rating:", "XXX")
         right_text = "<span style='font-family: sans-serif; color: #FAFAFA; font-size: 2.25em;'>{}&nbsp;{}</span>".format("Scenario Rating:", "XXX")
-
-        html2 = f"<div class='col'><div class='left'>{left_text}</div><div class='right'>{right_text}</div></div>"
-        st.markdown(html2, unsafe_allow_html=True)
-        
-        
-        left_text = "<span style='font-family: sans-serif; color: #FAFAFA; font-size: 44px;'>{}&nbsp;{}&nbsp;{}</span>".format(str(st.session_state.user_reporting_period), "Rating:", "XXX")
-        right_text = "<span style='font-family: sans-serif; color: #FAFAFA; font-size: 44px;'>{}&nbsp;{}</span>".format("Scenario Rating:", "XXX")
-
-        html = f"<div class='col'><div class='left'>{left_text}</div><div class='right'>{right_text}</div></div>"
-        st.markdown(html, unsafe_allow_html=True)
+        html4 = f"<div class='col'><div class='left'>{left_text}</div><div class='right'>{right_text}</div></div>"
+        st.markdown(html4, unsafe_allow_html=True)
         text = '<p style="margin-top: 20px; margin-bottom: 10px; text-align: justify;"><span style="color: #25476A; background-color: rgba(3, 169, 244, 0.2); border-radius:6px; padding-left:12px; padding-right: 12px; padding-top:12px; padding-bottom:12px; font-family:sans-serif; font-size: 24px; display: block; width: 100%; border: 3px solid #25476A; font-weight: bold;">Comrate&apos;s proprietary credit ratings model calculates a {} rating for {} at {} and predicts a {} rating based on the scenario provided.</span></p>'.format("XXX", st.session_state.user_entity_name, st.session_state.user_reporting_period, "XXX")
         st.markdown(text, unsafe_allow_html=True)
         col1, col2, col3, col4, col5 = st.columns([2, 1, 1, 1, 1])
