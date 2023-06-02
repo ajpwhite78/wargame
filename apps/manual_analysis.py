e4c8159cb38433a90de0906c908d0f3a1d201c8b
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-02 10:14:44 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -1246,7 +1246,7 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
             @media (max-width: 600px) {
                .col2 {
                     margin-top: -5em;
-                    padding-right: 2em;
+                    padding-right: 0em;
                 }
                 p.subtext_manual2 {
                     font-size: 2.5em;
