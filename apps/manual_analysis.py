7e8befb7ca7b59dc655bd30ce60c7625fe9deb87
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-02 13:57:50 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2001,14 +2001,16 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
         st.text("")
         st.markdown(styles, unsafe_allow_html=True)
         
-        left_text = "<span style='font-family: sans-serif; color: #FAFAFA; font-size: 2.25em;'>{}&nbsp;{}<span class='break-line'>&nbsp;{}</span>".format(str(st.session_state.user_reporting_period), "Rating:", "XXX")
-        right_text = "<span style='font-family: sans-serif; color: #FAFAFA; font-size: 2.25em;'>{}<span class='break-line'>&nbsp;{}</span>".format("Scenario Rating:", "XXX")
+        current_rating = "AA"
+        predict_rating = "BBB"
+        left_text = "<span style='font-family: sans-serif; color: #FAFAFA; font-size: 2.25em;'>{}&nbsp;{}<span class='break-line'>&nbsp;{}</span>".format(str(st.session_state.user_reporting_period), "Rating:", current_rating)
+        right_text = "<span style='font-family: sans-serif; color: #FAFAFA; font-size: 2.25em;'>{}<span class='break-line'>&nbsp;{}</span>".format("Scenario Rating:", predict_rating)
         html4 = f"<div class='col'><div class='left'>{left_text}</div><div class='right'>{right_text}</div></div>"
         st.markdown(html4, unsafe_allow_html=True)      
 
         text1 = '''
         <p class="text1" style="margin-top: 1em; margin-bottom: 1em; text-align: justify;"><span style="color: #25476A; background-color: rgba(3, 169, 244, 0.2); border-radius: 0.375em; padding-left: 0.75em; padding-right: 0.75em; padding-top: 0.5em; padding-bottom: 0.5em; font-family: sans-serif; font-size: 1em; font-weight: bold; display: block; width: 100%; border: 0.1875em solid #25476A;">Paydar calculates a {} rating for {} at {} and predicts a {} rating based on the scenario provided.</span></p>
-        '''.format("XXX", st.session_state.user_entity_name, st.session_state.user_reporting_period, "XXX")
+        '''.format(current_rating, st.session_state.user_entity_name, st.session_state.user_reporting_period, predict_rating)
         st.markdown(text_media_query_manual1 + text1, unsafe_allow_html=True)
 
 
