2b97e07148eba0d368f3e3197ed63947721fa724
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-02 15:47:47 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -1306,24 +1306,12 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
                 padding-top: 0em;
                 padding-bottom: 0em;
             }
-            .break-line3 {
-                display: inline;
-                white-space: normal;
-            }
-
             @media (max-width: 600px) {
                 .col3 {
-                    border: 0.5em solid #03A9F4;
                     font-size: 2.25em;
-                    margin-bottom: 4em;
-                }
-                .break-line3 {
-                    display: block;
-                    white-space: pre-wrap;
                 }
             }
-        </style>
-        """
+        </style>        """
 
         st.markdown(styles3, unsafe_allow_html=True)
 
@@ -2124,8 +2112,8 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
 
         col1, col2, col3 = st.columns(3)
         with col1:
-            left_text = "<span style='font-family: sans-serif; color: #25476A; font-size: 2em;'>Income Statement</span>"
-            right_text = "<span style='font-family: sans-serif; color: #25476A; font-size: 1em;'>(blue fields indicate change)</span></span>"
+            left_text = "<span style='font-family: sans-serif; color: #25476A; font-size: 1.8em;'>Income Statement</span>"
+            right_text = "<span style='font-family: sans-serif; color: #25476A; font-size: 0.8em;'>(blue fields indicate change)</span></span>"
 
             html5 = f"<div class='col3'><div class='left3'>{left_text}</div><div class='right3'>{right_text}</div></div>"
             st.markdown(html5, unsafe_allow_html=True)
