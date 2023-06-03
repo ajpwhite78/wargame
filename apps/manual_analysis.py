b2ab103a21bcd737153dc3c7d29c745606eaeea0
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-03 16:52:29 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -1725,8 +1725,7 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
                 'bgcolor': 'rgba(0, 0, 0, 0)',
                 'borderwidth': 4,
                 'bordercolor': "#25476A"
-            },
-            valueformat='.1f'
+            }
         ))
 
         manual_current_rating_fig.update_layout(
