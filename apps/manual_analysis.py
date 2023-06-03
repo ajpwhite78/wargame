fee72dedfde3622255df07a8c2a2cbf247df443b
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-03 08:29:05 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -1803,12 +1803,9 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
                 "%s" % st.session_state.user_entity_name + " (" + df_financials['currency_iso'].values[
                     0] + " Millions)"], **{'text-align': 'left'}, **{'width': '400px'}).hide_index()
 
-        df_income_statement_out_png = st.session_state.df_income_statement_out.style.apply(
+        df_income_statement_out_png = st.session_state.df_income_statement_out.apply(
                 lambda row: highlight_diff_by_row(row, color1=(3, 169, 244, 0.5), color2=(0, 0, 0, 0)),
-                axis=1).set_properties(subset=["%s" % st.session_state.user_reporting_period, "Scenario"],
-                                       **{'text-align': 'center'}, **{'width': '120px'}).set_properties(subset=[
-                "%s" % st.session_state.user_entity_name + " (" + df_financials['currency_iso'].values[
-                    0] + " Millions)"], **{'text-align': 'left'}, **{'width': '400px'})
+                axis=1)
         st.markdown('<div style="margin-top: -11px">' + df_income_statement_out_png.to_html(), unsafe_allow_html=True)
         
         df_cash_flow_statement_out_png = st.session_state.df_cash_flow_statement_out.style.set_table_styles([{'selector': 'td',
