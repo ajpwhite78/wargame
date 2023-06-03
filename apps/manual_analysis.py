9f6d6fb9b4ca306a245bb6b8a9949598e7058c65
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-03 16:25:37 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -1713,10 +1713,10 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
                                                                    range(1)], shared_xaxes=True, horizontal_spacing=0.1,
                                                             vertical_spacing=0)
         manual_ratings_fig.add_trace(trace=go.Indicator(mode="gauge+number", value=5,
-                                                          domain={'x': [-0.5, 1], 'y': [0, 1]}, title={
+                                                          domain={'x': [0, 1], 'y': [0, 1]}, title={
                         'text': 'Credit Rating',
-                        'font': {'size': 42, 'color': "#25476A"}}, gauge={
-                        'axis': {'range': [0, 23], 'dtick': 1, 'tickwidth': 4,
+                        'font': {'size': "2em", 'color': "#25476A"}}, gauge={
+                        'axis': {'range': [0, 22], 'dtick': 1, 'tickwidth': 4,
                                  'tickcolor': "#25476A"}, 'shape': "angular",
                         'bar': {'color': "#25476A"}, 'bgcolor': 'rgba(0, 0, 0, 0)',
                         'borderwidth': 4,
@@ -1725,9 +1725,9 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
                                       'value': 0.9}}), row=1, col=1)
         manual_ratings_fig.add_trace(trace=go.Indicator(mode="gauge+number", value=5,
                                                           domain={'x': [0, 1], 'y': [0, 1]}, title={
-                        'text': 'Credit Rating',
-                        'font': {'size': 42, 'color': "#25476A"}}, gauge={
-                        'axis': {'range': [0, 23], 'dtick': 1, 'tickwidth': 4,
+                        'text': 'Scenario Rating',
+                        'font': {'size': "2em", 'color': "#25476A"}}, gauge={
+                        'axis': {'range': [0, 22], 'dtick': 1, 'tickwidth': 4,
                                  'tickcolor': "#25476A"}, 'shape': "angular",
                         'bar': {'color': "#25476A"}, 'bgcolor': 'rgba(0, 0, 0, 0)',
                         'borderwidth': 4,
