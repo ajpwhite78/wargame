932c5799b63de1b18aad2e84f9c53238cce07c28
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-04 14:10:57 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2143,7 +2143,6 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
             paper_bgcolor='rgba(0, 0, 0, 0)',
             font={'color': "#25476A", 'size': 20},
             width=400,
-            margin=dict(l=50, r=50, t=50, b=50)
         )
 
         manual_current_rating_fig.update_xaxes(color="#25476A", mirror=True, showline=True)
@@ -2193,8 +2192,8 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
             st.markdown('<div class="full-width-chart"><div class="chart-container">{}</div></div>'.format(manual_current_rating_fig.to_html(include_plotlyjs='cdn')), unsafe_allow_html=True)
   
         with col2:
-            manual_scenario_rating_fig.update_layout(height=100)
-            st.plotly_chart(manual_scenario_rating_fig, config={'displayModeBar': False}, use_container_width=True, height=100)
+            manual_scenario_rating_fig.update_layout(height=300)
+            st.plotly_chart(manual_scenario_rating_fig, config={'displayModeBar': False}, use_container_width=True, height=300)
             st.markdown("<style>.full-width-chart .chart-container { display: flex; justify-content: center; margin-bottom: -2000em; }</style>", unsafe_allow_html=True)
             st.markdown('<div class="full-width-chart"><div class="chart-container">{}</div></div>'.format(manual_scenario_rating_fig.to_html(include_plotlyjs='cdn')), unsafe_allow_html=True)
 
