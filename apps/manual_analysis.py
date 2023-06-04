257b854ef3214965567718973a6568e302a60c91
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-03 17:01:34 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -1719,7 +1719,7 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
             },
             gauge={
                 'axis': {'range': [0, 22], 'dtick': 1, 'tickwidth': 4, 'tickcolor': "#25476A",
-                         'ticktext': ['AAA', 'AA+', 'AA', 'AA-', 'A+', 'A', 'A-', 'BBB+', 'BBB', 'BBB-', 'BB+', "BB", 'BB-', 'B+', 'B', 'B-', 'CCC+', 'CCC', 'CCC-', 'CC', 'C', 'D'], 'tickvals': [0.5, 1.5, 2.5, 3.3]},
+                         'ticktext': ['AAA', 'AA+', 'AA', 'AA-', 'A+', 'A', 'A-', 'BBB+', 'BBB', 'BBB-', 'BB+', "BB", 'BB-', 'B+', 'B', 'B-', 'CCC+', 'CCC', 'CCC-', 'CC', 'C', 'D'], 'tickvals': [0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.5, 12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5, 20.5, 21.5]},
                 'shape': "angular",
                 'bar': {'color': "#25476A"},
                 'bgcolor': 'rgba(0, 0, 0, 0)',
