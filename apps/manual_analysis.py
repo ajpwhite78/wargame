9c530012a9579e8f1f64801bbf3f5c6bd0eea09f
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-04 16:11:39 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2187,7 +2187,7 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
 
         manual_scenario_rating_fig.update_xaxes(color="#25476A", mirror=True, showline=True) 
         
-        col1, col2 = st.columns(2)
+        col1, col2, col3 = st.columns(3)
         with col1:
             st.plotly_chart(manual_current_rating_fig, config={'displayModeBar': False}, use_container_width=True)
        #     st.markdown('<div class="full-width-chart"><div class="chart-container">{}</div></div>'.format(manual_current_rating_fig.to_html(include_plotlyjs='cdn')), unsafe_allow_html=True)
