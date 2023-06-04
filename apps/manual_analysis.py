62c5eb4961ab09262493df3c235b6475439274cb
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-04 16:04:24 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2142,7 +2142,7 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
         manual_current_rating_fig.update_layout(
             paper_bgcolor='rgba(0, 0, 0, 0)',
             font={'color': "#25476A", 'size': 14},
-            width=220,
+            width=300,
             height=300
         )
 
@@ -2181,7 +2181,7 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
         manual_scenario_rating_fig.update_layout(
             paper_bgcolor='rgba(0, 0, 0, 0)',
             font={'color': "#25476A", 'size': 14},
-            width=220,
+            width=300,
             height=300
         )
 
