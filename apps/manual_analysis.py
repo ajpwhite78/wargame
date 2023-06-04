912f5beca9322cb344d51f53aa504de81f6e42d5
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-04 14:59:45 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2196,7 +2196,7 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
         #    st.markdown('<div class="full-width-chart"><div class="chart-container">{}</div></div>'.format(manual_scenario_rating_fig.to_html(include_plotlyjs='cdn')), unsafe_allow_html=True)
 
             
-       # st.markdown("<style>.full-width-chart .chart-container { display: flex; justify-content: center; }</style>", unsafe_allow_html=True)
+        st.markdown("<style>.full-width-chart .chart-container { display: flex; justify-content: center; }</style>", unsafe_allow_html=True)
         subtext1A = '<p class="subtext" style="margin-bottom: 0em;"><span style="font-family:sans-serif; color:#25476A; font-size: 2em;">Financial Statements</span></p>'
         text_media_query_manual2 = '''
             <style>
