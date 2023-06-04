fd0d9606dcdf981e340b250df47d7c21ec700e56
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-04 13:38:37 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2197,7 +2197,17 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
             st.markdown("<style>.full-width-chart .chart-container { display: flex; justify-content: center; }</style>", unsafe_allow_html=True)
             st.markdown('<div class="full-width-chart"><div class="chart-container">{}</div></div>'.format(manual_scenario_rating_fig.to_html(include_plotlyjs='cdn')), unsafe_allow_html=True)
 
-        
+        col1, col2 = st.columns(2)
+        with col1:
+            st.plotly_chart(manual_current_rating_fig, config={'displayModeBar': False}, use_container_width=True)
+            st.markdown("<style>.full-width-chart .chart-container { display: flex; justify-content: center; margin-top: -20px; }</style>", unsafe_allow_html=True)
+            st.markdown('<div class="full-width-chart"><div class="chart-container">{}</div></div>'.format(manual_current_rating_fig.to_html(include_plotlyjs='cdn')), unsafe_allow_html=True)
+
+        with col2:
+            st.plotly_chart(manual_scenario_rating_fig, config={'displayModeBar': False}, use_container_width=True)
+            st.markdown("<style>.full-width-chart .chart-container { display: flex; justify-content: center; margin-top: -20px; }</style>", unsafe_allow_html=True)
+            st.markdown('<div class="full-width-chart"><div class="chart-container">{}</div></div>'.format(manual_scenario_rating_fig.to_html(include_plotlyjs='cdn')), unsafe_allow_html=True)
+
         
 
         subtext1A = '<p class="subtext" style="margin-bottom: 0em;"><span style="font-family:sans-serif; color:#25476A; font-size: 2em;">Financial Statements</span></p>'
