5bb234af46c14cec22997fb4262bb4f9d53327f5
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-04 14:51:36 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2200,7 +2200,7 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
 
             
 
-        subtext1A = '<p class="subtext" style="margin-bottom: 0em;"><span style="font-family:sans-serif; color:#25476A; font-size: 2em;">Financial Statements</span></p>'
+        subtext1A = '<p class="subtext" style="margin-top: -10em; margin-bottom: 0em;"><span style="font-family:sans-serif; color:#25476A; font-size: 2em;">Financial Statements</span></p>'
         text_media_query_manual2 = '''
             <style>
             @media (max-width: 600px) {
