4e72c6a6002ee18a07713ac243a9bceeee96aad9
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-04 08:07:36 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2145,9 +2145,8 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
                 'steps': [{'range': [step_values[i], step_values[i + 1]], 'color': step_colors[i]} for i in range(num_steps - 1)],
                 'borderwidth': 8,
                 'bordercolor': "#25476A",
-                'threshold': {'line': {'color': "#25476A", 'width': 20}, 'thickness': 0.75,
-                                                          'value': 21.5-(current_rating_value-1)}
-            }
+
+            },
             threshold={'line': {'color': "#25476A", 'width': 20}, 'thickness': 0.75, 'value': 21.5 - (current_rating_value - 1)}
         ))
 
