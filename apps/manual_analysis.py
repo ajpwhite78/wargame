2e606eaa492202a5e4f5f571d65ba50013e3895f
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-04 14:52:31 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2191,16 +2191,16 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
         with col1:
             st.plotly_chart(manual_current_rating_fig, config={'displayModeBar': False}, use_container_width=True, height=340)
             st.markdown("<style>.full-width-chart .chart-container { display: flex; justify-content: center;}</style>", unsafe_allow_html=True)
-            st.markdown('<div class="full-width-chart"><div class="chart-container">{}</div></div>'.format(manual_current_rating_fig.to_html(include_plotlyjs='cdn')), unsafe_allow_html=True)
+       #     st.markdown('<div class="full-width-chart"><div class="chart-container">{}</div></div>'.format(manual_current_rating_fig.to_html(include_plotlyjs='cdn')), unsafe_allow_html=True)
   
         with col2:
             st.plotly_chart(manual_scenario_rating_fig, config={'displayModeBar': False}, use_container_width=True, height=340)
             st.markdown("<style>.full-width-chart .chart-container { display: flex; justify-content: center; }</style>", unsafe_allow_html=True)
-            st.markdown('<div class="full-width-chart"><div class="chart-container">{}</div></div>'.format(manual_scenario_rating_fig.to_html(include_plotlyjs='cdn')), unsafe_allow_html=True)
+        #    st.markdown('<div class="full-width-chart"><div class="chart-container">{}</div></div>'.format(manual_scenario_rating_fig.to_html(include_plotlyjs='cdn')), unsafe_allow_html=True)
 
             
 
-        subtext1A = '<p class="subtext" style="margin-top: -10em; margin-bottom: 0em;"><span style="font-family:sans-serif; color:#25476A; font-size: 2em;">Financial Statements</span></p>'
+        subtext1A = '<p class="subtext" style="margin-bottom: 0em;"><span style="font-family:sans-serif; color:#25476A; font-size: 2em;">Financial Statements</span></p>'
         text_media_query_manual2 = '''
             <style>
             @media (max-width: 600px) {
