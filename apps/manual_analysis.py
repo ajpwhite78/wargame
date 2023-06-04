e723b316415b0bbcf4ddbebc053a14fb6c0e178f
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-03 17:18:34 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2101,10 +2101,6 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
             mode="gauge",
             value=current_rating_value+0.5,
             domain={'x': [0, 1], 'y': [0, 1]},
-            title={
-                'text': 'Credit Rating',
-                'font': {'size': 10, 'color': "#25476A", 'family': 'sans-serif'}
-            },
             gauge={
                 'axis': {'range': [0, 22], 'dtick': 1, 'tickwidth': 4, 'tickcolor': "#25476A",
                          'ticktext': ['AAA', 'AA+', 'AA', 'AA-', 'A+', 'A', 'A-', 'BBB+', 'BBB', 'BBB-', 'BB+', "BB", 'BB-', 'B+', 'B', 'B-', 'CCC+', 'CCC', 'CCC-', 'CC', 'C', 'D'], 'tickvals': [0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.5, 12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5, 20.5, 21.5], 'tickangle': 0},
@@ -2117,10 +2113,10 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
         ))
 
         manual_current_rating_fig.add_annotation(
-    dict(font=dict(color="#25476A", size=14, family="sans-serif"), x=0.2, y=0.5,
+    dict(font=dict(color="#25476A", size=30, family="sans-serif"), x=0.2, y=0.5,
          xref="paper",
          yref="paper", showarrow=False,
-         text="Current Rating {}".format(current_rating)))
+         text="{} Rating {}".format(st.session_state.user_reporting_period, current_rating)))
 
         
         manual_current_rating_fig.update_layout(
