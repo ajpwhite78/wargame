1454ace1596ec8e257effe82e5c839102f0fa00b
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-04 13:25:34 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2141,7 +2141,7 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
          text="{}<br>Rating<br>{}".format(st.session_state.user_reporting_period, current_rating)))
         manual_current_rating_fig.update_layout(
             paper_bgcolor='rgba(0, 0, 0, 0)',
-            font={'color': "#25476A", 'size': 20}
+            font={'color': "#25476A", 'size': 20},
             width=600,
             margin=dict(l=20, r=20, t=20, b=20)
         )
