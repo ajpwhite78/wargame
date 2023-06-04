5315b09f29398eb3ab37dd17ef4876477425cacb
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-04 08:04:57 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2148,6 +2148,7 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
                 'threshold': {'line': {'color': "#25476A", 'width': 20}, 'thickness': 0.75,
                                                           'value': 21.5-(current_rating_value-1)}
             }
+            threshold={'line': {'color': "#25476A", 'width': 20}, 'thickness': 0.75, 'value': 21.5 - (current_rating_value - 1)}
         ))
 
         manual_current_rating_fig.add_annotation(
