559e33efc21f99596ad2acaef6a5f1844be2de9a
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-04 16:16:20 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2142,8 +2142,8 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
         manual_current_rating_fig.update_layout(
             paper_bgcolor='rgba(0, 0, 0, 0)',
             font={'color': "#25476A", 'size': 14},
-            width=340,
-            height=340
+            width=350,
+            height=350
         )
 
         manual_current_rating_fig.update_xaxes(color="#25476A", mirror=True, showline=True)
@@ -2181,23 +2181,19 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
         manual_scenario_rating_fig.update_layout(
             paper_bgcolor='rgba(0, 0, 0, 0)',
             font={'color': "#25476A", 'size': 14},
-            width=340,
-            height=340
+            width=350,
+            height=350
         )
 
         manual_scenario_rating_fig.update_xaxes(color="#25476A", mirror=True, showline=True) 
         
-        col1, col2, col3 = st.columns(3)
+        col1, col2 = st.columns(2)
         with col1:
             st.plotly_chart(manual_current_rating_fig, config={'displayModeBar': False}, use_container_width=True)
-       #     st.markdown('<div class="full-width-chart"><div class="chart-container">{}</div></div>'.format(manual_current_rating_fig.to_html(include_plotlyjs='cdn')), unsafe_allow_html=True)
-  
+ 
         with col2:
             st.plotly_chart(manual_scenario_rating_fig, config={'displayModeBar': False}, use_container_width=True)
-
-        #    st.markdown('<div class="full-width-chart"><div class="chart-container">{}</div></div>'.format(manual_scenario_rating_fig.to_html(include_plotlyjs='cdn')), unsafe_allow_html=True)
-
-            
+          
         st.markdown("<style>.full-width-chart .chart-container { display: flex; justify-content: center; }</style>", unsafe_allow_html=True)
         subtext1A = '<p class="subtext" style="margin-bottom: 0em;"><span style="font-family:sans-serif; color:#25476A; font-size: 2em;">Financial Statements</span></p>'
         text_media_query_manual2 = '''
