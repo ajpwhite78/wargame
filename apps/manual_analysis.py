944550254ca199ac6c9e67ca3f543c49abbf37de
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-05 11:09:09 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2253,8 +2253,6 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
             st.table(df_income_statement_out_png)
             #st.markdown('<div style="margin-top: -11px">' + df_income_statement_out_png.to_html(), unsafe_allow_html=True)
             st.text("")
-            st.text("")
-            st.text("")
         with col2:
             left_text = "<span style='font-family: sans-serif; color: #25476A; font-size: 1.7em;'>Cash Flow Statement</span>"
             right_text = "<span style='font-family: sans-serif; color: #25476A; font-size: 0.8em;'>(blue fields indicate change)</span></span>"
@@ -2265,8 +2263,6 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
             st.table(df_cash_flow_statement_out_png)
             #st.markdown('<div style="margin-top: -11px">' + df_cash_flow_statement_out_png.to_html(), unsafe_allow_html=True)
             st.text("")
-            st.text("")
-            st.text("")
         with col3:
             left_text = "<span style='font-family: sans-serif; color: #25476A; font-size: 1.7em;'>Balance Sheet</span>"
             right_text = "<span style='font-family: sans-serif; color: #25476A; font-size: 0.8em;'>(blue fields indicate change)</span></span>"
@@ -2276,8 +2272,6 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
             st.markdown(line_media_query3 + line3, unsafe_allow_html=True)
             st.table(df_balance_sheet_out_png)
             #st.markdown('<div style="margin-top: -11px">' + df_balance_sheet_out_png.to_html(), unsafe_allow_html=True)
-            st.text("")
-            st.text("")
-            st.text("")
+
 
             
