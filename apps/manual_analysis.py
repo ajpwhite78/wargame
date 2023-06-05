921fae81d8e3cd0ed17afee87e8c6a8af95d8728
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-05 05:07:46 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -1076,7 +1076,7 @@ def get_financials(datafame, user_entity_name, user_period):
 def app():
     line1 = '<hr class="line1" style="height:0.3em; border:0em; background-color: #03A9F4; margin-top: 0em;">'
     line2 = '<hr class="line2" style="height:0.1875em; border:0em; background-color: #25476A; margin-top: 0.2em;">'
-    line3 = '<hr class="line3" style="height:0.1875em; border:0em; background-color: #03A9F4; margin-top: -0.3125em; margin-bottom: -4.25em;">'
+    line3 = '<hr class="line3" style="height:0.1875em; border:0em; background-color: #03A9F4; margin-top: -0.3125em; margin-bottom: -10em !important;">'
     line_media_query1 = '''
         <style>
         @media (max-width: 600px) {
