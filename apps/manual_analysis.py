f94e8b0d45d1d8406a8542405907c31539044d9b
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-05 04:54:25 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -1809,19 +1809,7 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
                     0] + " Millions)"], **{'text-align': 'left'}, **{'width': '400px'}).hide_index()
 
         st.table(df_income_statement_out_png)
-        st._legacy_dataframe(df_income_statement_out_png)
-        st.markdown(df_income_statement_out_png.to_html(table_uuid="table_1"), unsafe_allow_html=True)
-        
-        from IPython.display import display, HTML
-        display(HTML(df_income_statement_out_png.to_html(index=False)))
-        df_income_statement_out_png = st.session_state.df_income_statement_out.style.apply(
-                lambda row: highlight_diff_by_row(row, color1=(3, 169, 244, 0.5), color2=(0, 0, 0, 0)),
-                axis=1)
-        st.markdown('<div style="margin-top: -11px">' + df_income_statement_out_png.to_html(index=False), unsafe_allow_html=True)
-        df_income_statement_out_png = st.session_state.df_income_statement_out.apply(
-                lambda row: highlight_diff_by_row(row, color1=(3, 169, 244, 0.5), color2=(0, 0, 0, 0)),
-                axis=1)
-      #  st.markdown('<div style="margin-top: -11px">' + df_income_statement_out_png.to_html(), unsafe_allow_html=True)
+
         
         df_cash_flow_statement_out_png = st.session_state.df_cash_flow_statement_out.style.set_table_styles([{'selector': 'td',
                                                                                           'props': [('color', '#25476A')]},{
