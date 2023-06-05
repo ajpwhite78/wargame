700007114f6253af2855a4e1a5c88d3aeda7a060
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-05 12:40:53 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -2255,8 +2255,8 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
             #st.markdown('<div style="margin-top: -11px">' + df_income_statement_out_png.to_html(), unsafe_allow_html=True)
             st.text("")
         with col2:
-            left_text = "<span style='font-family: sans-serif; color: #25476A; font-size: 1.7em;'>Cash Flow Statement</span>"
-            right_text = "<span style='font-family: sans-serif; color: #25476A; font-size: 0.8em;'>(blue fields indicate change)</span></span>"
+            left_text = "<span style='margin-bottom: 0em; font-family: sans-serif; color: #25476A; font-size: 1.7em;'>Cash Flow Statement</span>"
+            right_text = "<span style='margin-bottom: 0em; font-family: sans-serif; color: #25476A; font-size: 0.8em;'>(blue fields indicate change)</span></span>"
 
             html5 = f"<div class='col3'><div class='left3'>{left_text}</div><div class='right3'>{right_text}</div></div>"
             st.markdown(html5, unsafe_allow_html=True)
@@ -2265,8 +2265,8 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
             #st.markdown('<div style="margin-top: -11px">' + df_cash_flow_statement_out_png.to_html(), unsafe_allow_html=True)
             st.text("")
         with col3:
-            left_text = "<span style='font-family: sans-serif; color: #25476A; font-size: 1.7em;'>Balance Sheet</span>"
-            right_text = "<span style='font-family: sans-serif; color: #25476A; font-size: 0.8em;'>(blue fields indicate change)</span></span>"
+            left_text = "<span style='margin-bottom: 0em; font-family: sans-serif; color: #25476A; font-size: 1.7em;'>Balance Sheet</span>"
+            right_text = "<span style='margin-bottom: 0em; font-family: sans-serif; color: #25476A; font-size: 0.8em;'>(blue fields indicate change)</span></span>"
 
             html5 = f"<div class='col3'><div class='left3'>{left_text}</div><div class='right3'>{right_text}</div></div>"
             st.markdown(html5, unsafe_allow_html=True)
