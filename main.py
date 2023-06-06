a9aac819144b4956621d0c52bc3074a724fe720d
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-06 13:34:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1724,6 +1724,20 @@ input[type=number]:hover {
 </style>
 """, unsafe_allow_html=True)
 
+st.markdown(
+"""
+<style>
+.stMultiSelect [data-baseweb="tag"] {
+  height: fit-content;
+}
+.stMultiSelect [data-baseweb="tag"] span[title] {
+  white-space: normal;
+  max-width: 100%;
+  overflow-wrap: anywhere;
+}
+</style>
+""", unsafe_allow_html=True)
+
 introduction_text = '''
     <p class="introduction_text" style="margin-top: -1.25em; margin-bottom: 1.25em; text-align: justify;"><span style="color: #25476A; background-color: rgba(3, 169, 244, 0.2); border-radius: 0.375em; padding-left: 0.75em; padding-right: 0.75em; padding-top: 0.5em; padding-bottom: 0.5em; font-family: sans-serif; font-size: 1em; font-weight: bold; display: block; width: 100%; border: 0.1875em solid #25476A;">Welcome to the Paydar wargame scenario analysis application, empowering you to make informed decisions regarding the current and future financial performance of target companies based on manual and scenario-based financial statement and credit rating analyses.</span></p>
 '''
