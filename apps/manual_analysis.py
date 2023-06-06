4bdfdcac7c9f9e393a5d256e05930ff43e61c952
ajpwhite78 <74326214+ajpwhite78@users.noreply.github.com>
2023-06-05 19:50:57 -0700

    Update manual_analysis.py

--- apps/manual_analysis.py
+++ apps/manual_analysis.py
@@ -1513,7 +1513,7 @@ select_user_whatif_cost_of_goods_sold_margin=st.session_state.default_whatif_cos
             st.markdown(text_media_query_manual4 + text, unsafe_allow_html=True)
             user_whatif_capital_expenditure_indicator_field = st.empty()
             st.session_state.user_whatif_capital_expenditure_indicator = user_whatif_capital_expenditure_indicator_field.selectbox(label="", label_visibility="collapsed", options=["Dollar", "Sales %"], key="whatif_manual_14")
-            text = '<p class="heading_text4A" style="margin-top: 0em; margin-bottom: 0em;"><span style="font-family:sans-serif; color:#25476A; font-size: 0.9em; font-weight: bold;">Capex Tangible / Intangible Split %</span></p>'
+            text = '<p class="heading_text4A" style="margin-top: 0em; margin-bottom: 0em;"><span style="font-family:sans-serif; color:#25476A; font-size: 0.85em; font-weight: bold;">Capex Tangible / Intangible Split %</span></p>'
             st.markdown(text_media_query_manual4A + text, unsafe_allow_html=True)
             user_whatif_tangible_intangible_split_field = st.empty()
             st.session_state.user_whatif_tangible_intangible_split = user_whatif_tangible_intangible_split_field.number_input(label="", label_visibility="collapsed", min_value=0, max_value=100, step=None, format="%i", value=st.session_state.default_whatif_tangible_intangible_split_user_out, key="whatif_manual_15")
